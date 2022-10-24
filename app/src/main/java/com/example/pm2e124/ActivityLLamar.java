7913e1dfd0997ee9952a6b41e07aafc66461d6d1
johantorres504 <68003717+johantorres504@users.noreply.github.com>
2022-10-23 23:37:03 -0600

    PM2E124

--- app/src/main/java/com/example/pm2e124/ActivityLLamar.java
+++ app/src/main/java/com/example/pm2e124/ActivityLLamar.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -20,11 +21,52 @@ import com.example.pm2e124.tablas.Transacciones;
 import java.util.ArrayList;
 
 public class ActivityLLamar extends AppCompatActivity {
-
+        Button btnCompartir, btnImg,btnEliminar,btnActualizar;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_llamar);
 
+            btnCompartir = (Button) findViewById(R.id.btnCompartir);
+            btnImg = (Button) findViewById(R.id.btnImg);
+            btnEliminar = (Button) findViewById(R.id.btnEliminar);
+            btnActualizar = (Button) findViewById(R.id.btnActualizar);
+
+            btnCompartir.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view)
+                {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+
+            btnImg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view)
+                {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            btnEliminar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view)
+                {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            btnActualizar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view)
+                {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            });
         }
 }
